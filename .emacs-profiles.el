cd4d3c8fca6ad1ae23c33a41d984f8fcf1a0263a
Ezequiel Birman <ebirman77@gmail.com>
2020-03-13 02:03:08 -0300

    New Chemacs profiles

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.emacs.d")))
-
+ ("doom-emacs" . ((user-emacs-directory . "~/doom-emacs")))
  ("spacemacs" . ((user-emacs-directory . "~/spacemacs")
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d"))))))
