3c526561870e5ee47bbf0a8c15743b2db4fc49d4
Ezequiel Birman <ebirman77@gmail.com>
2024-12-04 15:50:05 +0000

    Cuis: fish loaad and autocompletion scripts

---
+++ .config/fish/completions/cuis.fish
@@ -0,0 +1,2 @@
+set prefix '/Users/eze/.local/src/Smalltalk/Cuis/Cuis-Smalltalk-Dev/CuisImage' 
+complete --command cuis --exclusive --keep-order --no-files --arguments '(__fish_complete_suffix .image --prefix=$prefix)'
---
+++ .config/fish/completions/cuisuniversity.fish
@@ -0,0 +1,2 @@
+set prefix '/Users/eze/.local/src/Smalltalk/Cuis\ University'
+complete --command cuisuniversity --exclusive --keep-order --no-files --arguments '(__fish_complete_suffix .image --prefix=$prefix)'
