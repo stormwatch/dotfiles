77e003d0ac11fb290da46a0b96a89e2ae61d2359
Ezequiel Birman <ebirman77@gmail.com>
2025-09-30 10:54:17 +0100

    Remove fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,8 @@
 autoLoadEz3Functions
 if status is-interactive
-  fzfConfiguration
   itermConfiguration
   allAbbreviations
   interactiveMiseConfiguration
 else
   nonInteractiveMiseConfiguration
 end
-
-
